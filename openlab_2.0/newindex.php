bb54fbc88a6d5f79c118c448c288acb9836a7670
Marian Kapitan <145431830+Mr-Marian@users.noreply.github.com>
2025-03-27 17:23:18 +0100

    Nová verzia vypracovania stránky openlab
    Na základe review som zhodnotil že najlepšie bude začať znovu a viac premyslene.

---
+++ openlab_2.0/newindex.php
@@ -0,0 +1,332 @@
+<?php include "./_partials/header.php"; ?>
+
+    <div class="container">
+        
+        <div class="mine-block">
+            <h1 class="mine-heading">Inštitút stredoškolského odborného vzdelávania<br> 
+                <span>zameraný na IT & AI</span>
+            </h1>
+            <p class="mine-p">Realizujeme odborné vzdelávanie zamerané na IT pre štátne školy ako nový štandard formálneho vzdelávania</p>
+        </div>
+
+        <img class="main-home" src="./img/main-home.webp" alt="students">
+            
+        <img class="circle" src="./img/o_letter.webp" alt="circle">
+        
+    </div>
+
+    <div class="container">
+        <section id="why">
+            <h1>Prečo <span>OpenLab?</span></h1>
+            <p class="why">
+                V OpenLabe študentov učíme najnovšie technológie. Zároveň im dávame veľkú slobodu, 
+                pričom ich vedieme k zodpovednosti a samostatnosti, učíme ich princípy partnerského 
+                prístupu, pracovať v tíme, trénujeme ich schopnosti sebareflexie a podnecujeme ich 
+                kreativitu. Takto študentov pripravujeme osobnostne aj tech zručnosťami na veľké veci, 
+                ktoré môžu v budúcnosti tvoriť.
+            </p>
+            <div class="openlab-education-method">
+                <aside>
+                    <img src="https://openlab.sk/wp-content/themes/wp-bootstrap-starter/assets/images/select.svg" alt="select">
+                    <h2>Celospoločenský rozvoj</h2>
+                    <p>Osnovy aj učebné materiály pre všetky tech oblasti sú open source.</p>
+                </aside>
+                <aside>
+                    <img src="https://openlab.sk/wp-content/themes/wp-bootstrap-starter/assets/images/support.svg" alt="support">
+                    <h2>Učenie praxou</h2>
+                    <p>Študentov vedieme k tímovému riešeniu problémov.</p>
+                </aside>
+                <aside>
+                    <img src="https://openlab.sk/wp-content/themes/wp-bootstrap-starter/assets/images/intership.svg" alt="internship">
+                    <h2>Rozvoj talentu</h2>
+                    <p>Vytvárame šikovné tímy schopné riešiť veľké problémy.</p>
+                </aside>
+            </div>
+        </section>
+    </div>
+
+    <div class="container">
+        <div class="container-vision">
+            <section class="text-box">
+                <h1 class="vision-heading">Naša vízia <span>v OpenLabe</span></h1>
+                <p class="vision-p">
+                    Veríme, že otvorený prístup a kvalitné vzdelanie 
+                    sú základom úspechu jednotlivca ako aj prosperujúcej 
+                    spoločnosti.
+                </p>
+                <p class="vision-p height-72">
+                    Uvedomujeme si, že ak chceme niečo zmeniť, musíme ísť 
+                    príkladom. Našim cieľom je rozšíriť princípy OpenLab-u 
+                    na všetkých úrovniach škôl a nastaviť svetový štandard 
+                    vzdelávania v školstve v technológiách.
+                </p>
+            </section>
+            <section class="color-boxes">
+                <div class="color-box one">
+                    <p class="number regular">7</p>
+                    <p class="text regular">tried</p>
+                </div>
+                <div class="color-box two">
+                    <p class="number regular">330</p>
+                    <p class="text regular">študentov</p>
+                </div>
+                <div class="color-box three">
+                    <p class="number regular">25</p>
+                    <p class="text regular">LabMastrov</p>
+                </div>
+                <div class="color-box four">
+                    <p class="number regular">2</p>
+                    <p class="text regular">školy</p>
+                </div>
+            </section>
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="contain">
+            <div class="labs-heading">
+                <h1 class="labs">V akých Laboch <span>možeš študovať?</span></h1>
+            </div>
+            <div class="labs-aside">
+                <div class="cards-group">
+                    <div class="lab lab-first">
+                        <div class="card-size">
+                            <h2 class="weight-100"><b>Hybrid</b>Lab</h2>
+                            <p class="lab-text">
+                                Študenti sa učia vyvíjať hybridné aplikácie v HTML, 
+                                PHP, CSS, JavaScript, Ionicu, Vue a OctoberCMS.
+                            </p>
+                            <p class="arrow-right arrow-size">→</p>
+                        </div>
+                    </div>
+                    <div class="lab lab-second">
+                        <div class="card-size">
+                            <h2 class="weight-100"><b>Games</b>Lab</h2>
+                            <p class="lab-text">
+                                Študenti vyvíjajú mobilné hry v Unity a GDevelop, 
+                                učia sa tvoriť 2D grafiku v programe Piskel, 3D grafiku 
+                                v programe Blender a herný dizajn.
+                            </p>
+                            <p class="arrow-size">→</p>
+                        </div>
+                    </div>
+                    <div class="lab">
+                        <div class="card-size">
+                            <h2 class="weight-100"><b>Apps</b>Lab</h2>
+                            <p class="lab-text">
+                                Študenti sa učia vyvíjať mobilné aplikácie v C# a 
+                                Xamarin.forms a webové aplikácie v Java a Angular JS.
+                            </p>
+                            <p class="arrow-right arrow-size">→</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="education-center">
+            <div class="contain contain-center">
+                <h1 class="center-heading">Technologicko-vzdelávacie <span>centrum</span></h1>
+                <p>
+                    V rámci formálneho vzdelávania predstavuje nový model 
+                    OpenLabu – Technologicko-vzdelávacie centrum, ktoré umožňuje 
+                    študentom posunúť sa do moderných a stimulujúcich vzdelávacích 
+                    priestorov. Tieto centrá sú navrhnuté s cieľom poskytnúť mimoriadne 
+                    talentovaným a motivovaným stredoškolákom bezpečné a inšpiratívne 
+                    prostredie na ich ďalší rozvoj.
+                </p>
+                <p>
+                    Umožňuje nám to zapojiť väčší počet stredných škôl do OpenLabu 
+                    a vďaka tomu sa otvára príležitosť pre svetové vzdelanie desiatkam 
+                    až stovkám ďalších študentov. Títo študenti získavajú mäkké 
+                    a digitálne zručnosti v technológiách 21. storočia.
+                </p>
+            </div>
+        </div>
+        <div class="center-equipment">
+            <h2>Vybavenie <span>technologického centra</span></h2>
+            <div class="center-box">
+                <img src="https://openlab.sk/wp-content/uploads/2024/01/tc-odbornici.svg" alt="team">
+                <p>Spolužiaci a odborníci z praxe</p>
+            </div>
+            <div class="center-box">
+                <img src="https://openlab.sk/wp-content/uploads/2024/01/tc-technologie.svg" alt="computer">
+                <p>Moderné technologické vybavenie</p>
+            </div>
+            <div class="center-box">
+                <img src="https://openlab.sk/wp-content/uploads/2024/01/tc-bezpecnost.svg" alt="shield">
+                <p>Bezpečné vzdelávacie prostredie</p>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="big-image">
+            <img class="image-big" src="https://openlab.sk/wp-content/uploads/2024/02/tc-1.svg" alt="classroom">
+        </div>
+        <img class="image-small" src="https://openlab.sk/wp-content/uploads/2024/02/tc-2.svg" alt="students-1">
+        <img class="image-small" src="https://openlab.sk/wp-content/uploads/2024/02/tc-3.svg" alt="students-2">
+        <div class="big-image">
+            <img class="image-big" src="https://openlab.sk/wp-content/uploads/2024/02/tc-4.svg" alt="teacher">
+        </div>
+    </div>
+    
+    <div class="container">
+        
+        <div class="block-width">
+            <h1 class="centers-heading">Naše <span class="padding-left">technologické centrá</span></h1>
+
+            <div class="centers">
+                
+                <div class="tech-center">
+                    <img src="https://openlab.sk/wp-content/uploads/2024/03/eurovea-final.svg" width="526" height="270" alt="eurovea">
+                    <div class="center-address">
+                        <img src="https://openlab.sk/wp-content/themes/wp-bootstrap-starter/assets/images/place.svg" alt="point">
+                        <p class="address">Pribinova 10, Bratislava</p>
+                    </div>
+                    <h2 class="location">Technologické srdce Bratislavy</h2>
+                </div>
+                
+                <div class="tech-center">
+                    <img src="https://openlab.sk/wp-content/uploads/2024/03/zilina-final.png" width="526" height="270" alt="zilina">
+                    <div class="center-address">
+                        <img src="https://openlab.sk/wp-content/themes/wp-bootstrap-starter/assets/images/place.svg" alt="point">
+                        <p class="address">Námestie A. Hlinku 2, Žilina</p>
+                    </div>
+                    <h2 class="location">Historické centrum Žiliny</h2>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+    
+    <div class="container">
+        <h1 class="scholls-heading"><span>Nachádzame sa </span>na dvoch školách</h1>
+
+            <div class="schools">
+
+                <div class="school">
+                    <img class="school-img-resize" src="https://openlab.sk/wp-content/uploads/2023/12/spsehalova.png" alt="school1">
+                    
+                    <div class="school-address">
+                        <img src="https://openlab.sk/wp-content/themes/wp-bootstrap-starter/assets/images/place.svg" alt="point">
+                        <p class="address">Hálova 16, Bratislava</p>
+                    </div>
+                    
+                    <div class="school-address align-end">
+                        <h2 class="school-info">Stredná Priemyselná Škola Elektrotechnická</h2>
+                        <a class="a-color" href="https://spsehalova.sk/">www.spsehalova.sk</a>
+                    </div>
+
+                </div>
+
+                <div class="school">
+
+                    <img class="school-img-resize" src="https://openlab.sk/wp-content/themes/wp-bootstrap-starter/assets/images/school2.png" alt="school2">
+                    
+                    <div class="school-address">
+                        <img src="https://openlab.sk/wp-content/themes/wp-bootstrap-starter/assets/images/place.svg" alt="point">
+                        <p class="address">Nábrežná 1325, Kysucké Nové Mesto</p>
+                    </div>
+                    
+                    <div class="school-address align-end">
+                        <h2 class="school-info">Stredná Priemyselná Škola Informačných Technológií</h2>
+                        <a class="a-color" href="https://www.spsknm.sk/ssknm/">www.spsknm.sk</a>
+                    </div>
+
+                </div>
+
+            </div>
+            
+            <div class="shape">
+                <img class="p-img" src="https://openlab.sk/wp-content/themes/wp-bootstrap-starter/assets/images/p_letter.png" alt="p-letter">
+            </div>
+    </div>
+    
+    <div class="container">
+        <div class="contain">
+                
+            <div class="row align-items-end">
+
+                <div class="students-box review-box">
+
+                    <h1 class="review-heading"><span>Čo si myslia</span> <br>o OpenLabe študenti?</h1>
+
+                    <div class="reviews">
+
+                        <div class="slider">
+
+                            <div class="slide first-student">
+                                <img class="quote" src="https://openlab.sk/wp-content/themes/wp-bootstrap-starter/assets/images/quote.svg" alt="quote">
+                                <p>
+                                    Openlab mi dal neuveriteľné množstvo skúsenosti do života, super ľudí, 
+                                    s ktorými vďaka stážam pracujem v rovnakej firme a samozrejme mnoho ďalších 
+                                    dôležitých veci pre fungovanie v dnešnej “IT” sfére. Prístup akým sa tu 
+                                    vyučuje je presne taký, aký v dnešnej dobe potrebujeme na stredných školách.
+                                </p>
+                                <div class="author">
+                                    <img class="person" src="https://openlab.sk/wp-content/themes/wp-bootstrap-starter/assets/images/icon-person.svg" alt="person">
+                                    <div class="name">
+                                        <h5 class="author-name">Marko</h5>
+                                        <p class="author-school">HybridLab, PDT</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="slide second-student">
+                                <img class="quote" src="https://openlab.sk/wp-content/themes/wp-bootstrap-starter/assets/images/quote.svg" alt="quote">
+                                <p>
+                                    Som veľmi rád, že som mal na strednej škole možnosť byť súčasťou OpenLabu 
+                                    a získavať vedomosti priamo od ľudí z praxe. Vďaka OpenLabu som už počas 
+                                    strednej skoly stážoval vo firme a naberal skúsenosti na projektoch, 
+                                    ktoré by som napríklad na vysokej škole ťažko naberal.
+                                </p>
+                                <div class="author">
+                                    <img src="https://openlab.sk/wp-content/themes/wp-bootstrap-starter/assets/images/icon-person.svg" alt="person">
+                                    <div class="name">
+                                        <h5 class="author-name">Marek</h5>
+                                        <p class="author-school">HybridLab, PDT</p>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div class="rew-nav">
+                            <button type="button" role="presentation" class="prev" aria-label="Previous">
+                              <i class="fas fa-arrow-left" style="font-size: 24px;"></i>
+                            </button>
+                            <button type="button" role="presentation" class="next" aria-label="Next">
+                              <i class="fas fa-arrow-right" style="font-size: 24px;"></i>
+                            </button>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="students-box">
+                    <img class="students-img" src="https://openlab.sk/wp-content/themes/wp-bootstrap-starter/assets/images/students3.png" alt="students">
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+    
+    <div class="container"></div>
+    
+    <div class="container"></div>
+    
+    <div class="container"></div>
+    
+    <div class="container"></div>
+    
+    <div class="container"></div>
+    
+    <div class="container"></div>
+</body>
+</html>
